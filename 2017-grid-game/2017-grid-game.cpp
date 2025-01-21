a0f961e3ff73a6c71baf45c9ec92fdc92f3692c7
Nitish Sharma <nitishsharma.sharma2@gmail.com>
2025-01-22 00:23:51 +0530

    Time: 4 ms (47.51%), Space: 66.6 MB (76.92%) - LeetHub

--- 2017-grid-game/2017-grid-game.cpp
+++ 2017-grid-game/2017-grid-game.cpp
@@ -1,46 +1,23 @@
-#include <algorithm>
 class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
-          long long n=grid[0].size();
-        vector<vector<long> > presum(2,vector<long> (n));
-       
-      
-        presum[0][0]=grid[0][0];
-        presum[1][0]=grid[1][0];
-
-      
-
-
-          for( int i =1; i < n;i++)
-          {
-              presum[0][i] =presum[0][i-1]+grid[0][i];
-               presum[1][i] =presum[1][i-1]+grid[1][i];
-
-              
-          }
-
-long long res=10000000000;
-
-
-         
-            for( int i =0; i < n;i++)
-          {
-              long long top= presum[0][n-1]-presum[0][i];
-              long long bottom=0;
-              if(i>0){
-                bottom=presum[1][i-1];
-              }
-              long long second=max(top,bottom);
-              res=min(res,second);
-              
-          }
-
-
-          return res;
+        int n = grid[0].size();
+        long long firstsum =0;
         
-
-
+        for( int i = 0 ; i < n ;i++){
+            firstsum += grid[0][i];
+        }
+        long long secondsum =0;
         
+        long long firstbotres =0 ;
+        long long res = LONG_LONG_MAX;
+        for( int i = 0 ; i < n ; i++){
+            firstsum -=grid[0][i];
+           firstbotres = max(secondsum , firstsum);
+           secondsum += grid[1][i];
+           res = min ( res , firstbotres);
+        }
+
+        return res;
     }
 };
