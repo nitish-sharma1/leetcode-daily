e2ec116ab4e0b1d45f93ae864388c1a11a3dd2d3
Nitish Sharma <nitishsharma.sharma2@gmail.com>
2025-02-15 16:44:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-    bool isPunish(int n ,string prod , int i ,int sum){
-        if(i == prod.size()){
-            return sum == n;
-        }
-        if(sum > n ){
+    bool isPunish(int n ,int prod , int sum){
+       if(prod == 0 ){
+        return sum == n ;
+       }
 
-            return false;
-        }
-        bool result = false;
-        for(int j = i ;  j < prod.size() ; j++ ){
-            string sub = prod.substr(i,j-i+1);
-            int val = stoi(sub);
-            result = result || isPunish(n,prod,j+1,sum+val);
-            if(result){
-                return true;
-            }
-        }
-
-        return false;
-
-        
+        return (isPunish(n , prod/10 , sum+prod%10) || isPunish(n,prod/100,sum + prod%100) || isPunish(n,prod/1000,sum + prod%1000) );
 
     }
     int punishmentNumber(int n) {
         int  res = 0 ; 
         for(int  i = 1 ; i <=n ; i++){
-            string prod = to_string(i*i);
-            if(isPunish(i , prod,0 ,0)){
+            if(isPunish(i , i*i,0)){
                 res += i*i;
             }
         }
