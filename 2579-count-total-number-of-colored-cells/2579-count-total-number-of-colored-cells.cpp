58ad0b8813b436b37b0f713af31fd1fd40ee363f
Nitish Sharma <nitishsharma.sharma2@gmail.com>
2025-03-05 23:02:34 +0530

    Time: 0 ms (100%), Space: 7.8 MB (91.36%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        if( n==1){
-            return 1;
-        }
-        return 4*(n-1)+coloredCells(n-1);
+      long long ans =1+ (long long)2*(n-1)*n;
+        return ans ;
     }
 };
