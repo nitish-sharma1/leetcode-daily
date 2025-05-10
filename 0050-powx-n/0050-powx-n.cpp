cc7e46d0fcb73d460800ffcee1f36ecd0ae8810c
Nitish Sharma <nitishsharma.sharma2@gmail.com>
2025-05-10 22:20:08 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.7%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     double solve(double x , long n){
-        if( n==0){
+        if( n ==0 ){
             return 1;
         }
-
-        if( n <0 ){
+        if(n < 0 ){
             return solve(1/x,-1*n);
         }
-        if(n%2 == 0){
+        if(n%2 ==0){
             return solve(x*x,n/2);
         }
-        else{
-            return x*solve(x*x,(n-1)/2);
-        }
- 
+        return x* solve(x*x,(n-1)/2);
     }
     double myPow(double x, int n) {
-        return solve(x,(long)n);
+        return solve(x,n);
     }
 };
