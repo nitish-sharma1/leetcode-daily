470238383a479377e996136c793f02319890b799
Nitish Sharma <nitishsharma.sharma2@gmail.com>
2025-05-11 19:20:50 +0530

    Time: 0 ms (100%), Space: 10.8 MB (65.29%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,18 +11,17 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root , vector<int> &inorder){
-        if(root == NULL){\
+void solve(TreeNode* root , vector<int> &inorder){
+    if(root==NULL){
         return;
-        }
-        solve(root->left,inorder);
-        inorder.push_back(root->val);
-        solve(root->right,inorder);
     }
+    solve(root->left , inorder);
+    inorder.push_back(root->val);
+    solve(root->right,inorder);
+}
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> inorder;
         solve(root,inorder);
         return inorder;
-        
     }
 };
