c34f3ead02b341767c9ed74c5581f26b30da304b
Nitish Sharma <nitishsharma.sharma2@gmail.com>
2025-05-11 21:56:45 +0530

    Time: 0 ms (100%), Space: 7.8 MB (71.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-   int t[46]={0};
+int dp[46]={0};
     int climbStairs(int n) {
-        if(n==0){
-            return 1;
-        }
-        if(n<0){
+        if(n<=0){
             return 0;
         }
-        if(t[n] != 0){
-            return t[n];
+        if(dp[n] !=0 ){
+            return dp[n];
         }
-        return t[n] = climbStairs(n-1) + climbStairs(n-2);
+        if(n==1){
+            return 1;
+        }
         
+        if(n==2){
+             return 2;
+        }
+        return dp[n] = climbStairs(n-1) + climbStairs(n-2);
     }
 };
