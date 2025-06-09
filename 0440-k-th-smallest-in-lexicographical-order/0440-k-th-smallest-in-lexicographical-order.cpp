9b3f7179d743cb181d5568e53434797b5d250ee8
Nitish Sharma <nitishsharma.sharma2@gmail.com>
2025-06-09 16:19:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void solve(int num , int n , vector<int> &res){
+        if(num>n){
+            return;
+        }
+        res.push_back(num);
+        for(int i=0;i<=9;i++){
+            int newnum = num*10 +i;
+            if(newnum>n){
+                return;
+            }
+            solve(newnum,n,res);
+        }
+    }
+    int findKthNumber(int n, int k) {
+        vector<int> res;
+        for(int i=1;i<=9;i++){
+            solve(i,n,res);
+        }
+        return res[k-1];
+    }
+};
